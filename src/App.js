2d399c227acb628342527116b3273b8a03a081f5
kateFrontend <ecdanilova@gmail.com>
2022-11-26 13:31:46 -0500

    install and setup router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Shop from "./Shop";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/home">Home</Link>
+        <Link to="/shop">Shop</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
